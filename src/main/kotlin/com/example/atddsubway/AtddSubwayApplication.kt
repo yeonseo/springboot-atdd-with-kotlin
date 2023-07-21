bf5c2db12b2f91f716c8ad8e012c315425627e3c
yeonseo <yulga_nara@naver.com>
2023-07-21 23:17:03 +0900

    feat: init commit

---
+++ src/main/kotlin/com/example/atddsubway/AtddSubwayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.atddsubway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtddSubwayApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtddSubwayApplication>(*args)
+}
