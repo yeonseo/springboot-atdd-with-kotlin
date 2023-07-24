86d900116d02e4823a65c0877134477cec0b17cb
yeonseo <yulga_nara@naver.com>
2023-07-25 00:07:56 +0900

    refactor: DatabaseCleanup ref

--- src/test/kotlin/com/example/atddsubway/DatabaseCleanup.kt
+++ src/test/kotlin/com/example/atddsubway/DatabaseCleanup.kt
@@ -1,59 +1,38 @@
 package com.example.atddsubway
 
+import com.google.common.base.CaseFormat
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import jakarta.persistence.metamodel.EntityType
+import jakarta.transaction.Transactional
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
-import java.util.stream.Collectors
+import org.springframework.test.context.ActiveProfiles
+
 
 @Component
-class DatabaseCleanup : InitializingBean {
+@ActiveProfiles("test")
+class DatabaseCleanup: InitializingBean {
     @PersistenceContext
-    private val entityManager: EntityManager? = null
-    private var tableNames: MutableList<String>? = null
+    private lateinit var entityManager: EntityManager
+
+    var tableNames: List<String>? = null
+
     override fun afterPropertiesSet() {
-        val entities: Set<EntityType<*>> = entityManager?.getMetamodel()?.getEntities() ?: return
-        tableNames = entities.stream()
-            .filter(Predicate<EntityType<*>> { e: EntityType<*> -> isEntity(e) && hasTableAnnotation(e) })
-            .map<String>(Function<EntityType<*>, String> { e: EntityType<*> ->
-                val tableName: String = e.getJavaType().getAnnotation(Table::class.java).name
-                if (tableName.isBlank()) CaseFormat.UPPER_CAMEL.to(
-                    CaseFormat.LOWER_UNDERSCORE,
-                    e.getName()
-                ) else tableName
-            })
-            .collect(Collectors.toList<String>())
-        val entityNames = entities.stream()
-            .filter(Predicate<EntityType<*>> { e: EntityType<*> -> isEntity(e) && !hasTableAnnotation(e) })
-            .map<String>(Function<EntityType<*>, String> { e: EntityType<*> ->
-                CaseFormat.UPPER_CAMEL.to(
-                    CaseFormat.LOWER_UNDERSCORE,
-                    e.getName()
-                )
-            })
-            .toList()
-        tableNames?.addAll(entityNames)
-    }
-
-    private fun isEntity(e: EntityType<*>): Boolean {
-        return null != e.getJavaType().getAnnotation(Entity::class.java)
-    }
-
-    private fun hasTableAnnotation(e: EntityType<*>): Boolean {
-        return null != e.getJavaType().getAnnotation(Table::class.java)
+        tableNames = entityManager!!.metamodel.entities
+            .filter { e: EntityType<*> -> e.javaType.getAnnotation(Entity::class.java) != null }
+            .map { e: EntityType<*> -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.name) }
     }
 
     @Transactional
     fun execute() {
-        /** TEST용 DB는 로컬에서 실행할 것!*/
-//        entityManager?.flush()
-//        entityManager?.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0")?.executeUpdate()
-//        for (tableName in tableNames!!) {
-//            if (tableName.equals("right", ignoreCase = true)) {
-//                entityManager?.createNativeQuery("DELETE FROM $tableName")?.executeUpdate()
-//            } else {
-//                entityManager?.createNativeQuery("TRUNCATE TABLE $tableName")?.executeUpdate()
-//            }
-//            entityManager?.createNativeQuery("ALTER TABLE $tableName AUTO_INCREMENT = 1")?.executeUpdate()
-//        }
-//        entityManager?.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1")?.executeUpdate()
+        entityManager.flush()
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate()
+        for (tableName in tableNames!!) {
+            entityManager.createNativeQuery("TRUNCATE TABLE $tableName").executeUpdate()
+            entityManager.createNativeQuery("ALTER TABLE $tableName ALTER COLUMN ID RESTART WITH 1").executeUpdate()
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate()
     }
 }
